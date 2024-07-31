0980c19630315ce68ffdb95f45b8ca8967eb09d2
Mauros Milach Junior <mauros99milach@gmail.com>
2024-07-30 22:21:59 -0300

    Fundamentals - Control structures - Activity 7

---
+++ Fundamentals - Control structures/Activity 7.sql
@@ -0,0 +1,13 @@
+set serveroutput on;
+
+ACCEPT wqtdrepeticao PROMPT 'Repetir quantas vezes?';
+
+DECLARE
+    wqtdrepeticao NUMBER(5) := &wqtdrepeticao;
+    wsum          NUMBER(5) := 0;
+BEGIN
+    while wsum < wqtdrepeticao LOOP
+       wsum := wsum +1; 
+        dbms_output.put_line('Linha: ' || wsum);
+    END LOOP;
+END;
