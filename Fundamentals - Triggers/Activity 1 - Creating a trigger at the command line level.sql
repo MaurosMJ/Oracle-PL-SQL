ba4183f6b8db515d2f73958accbd74ba30489265
Mauros Milach Junior <mauros99milach@gmail.com>
2024-08-04 19:00:28 -0300

    feat: Activity 1 - Creating a trigger at the command line level.sql

---
+++ Fundamentals - Triggers/Activity 1 - Creating a trigger at the command line level.sql
@@ -0,0 +1,13 @@
+--@Author: MaurosMJ
+
+CREATE OR REPLACE TRIGGER validar_horario_trg BEFORE
+    INSERT ON employees
+DECLARE BEGIN
+    IF ( TRIM(to_char(sysdate, 'DAY')) IN ( 'SABADO', 'DOMINGO' ) OR TO_NUMBER(to_char(sysdate, 'HH24')) NOT BETWEEN 8 AND 18 ) THEN
+        raise_application_error(-20001, 'Não é possível adicionar funcionarios fora do horario comercial.');
+    END IF;
+END;
+
+BEGIN
+  inserir_funcionario('George', 'Harrison','GHARRISON','515.258.5690',SYSDATE,'IT_PROG',25000,NULL,103,60);
+END;
