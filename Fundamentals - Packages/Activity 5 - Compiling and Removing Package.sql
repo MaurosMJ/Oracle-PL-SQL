a37f06b9be787d531239ff016caa7f010694a3af
Mauros Milach Junior <mauros99milach@gmail.com>
2024-08-04 16:33:07 -0300

    feat: Activity 5 - Compiling and Removing Package.sql

---
+++ Fundamentals - Packages/Activity 5 - Compiling and Removing Package.sql
@@ -0,0 +1,9 @@
+--Author: MaurosMJ
+
+alter package employee_pck compile specification; --RECOMPILA A PACKAGE SPECIFICATION
+
+alter package employee_pck compile body; --RECOMPILA A BODY PACKAGE
+
+drop package employee_pck; --ELIMINA A PACKAGE SPECIFICATION (E PACKAGE BODY)
+
+drop package body employee_pck; --ELIMINA A PACKAGE BODY (APENAS)
