f105471bdb029a748faed34cc2ea6f762b70b4d7
Mauros Milach Junior <mauros99milach@gmail.com>
2024-08-11 21:59:55 -0300

    feat: Activity 1 - Creating a program.sql

---
+++ Advanced - DBMS_SCHEDULER/Activity 1 - Creating a program.sql
@@ -0,0 +1,43 @@
+-- Grant create Job
+GRANT
+    CREATE JOB
+TO hr;
+
+--Criando uma tabela para testes
+CREATE TABLE agenda (
+    agenda_id   NUMBER,
+    agenda_data DATE
+);
+
+-- Criando sequence para a tabela
+CREATE SEQUENCE agenda_seq START WITH 1 INCREMENT BY 1 NOCACHE NOMAXVALUE NOCYCLE;
+
+-- Criando uma procedure para ser utilizada no programa
+CREATE OR REPLACE PROCEDURE inserir_agenda_prc (
+    p_agenda_data IN agenda.agenda_data%TYPE
+) AS
+BEGIN
+    INSERT INTO agenda (
+        agenda_id,
+        agenda_data
+    ) VALUES (
+        agenda_seq.NEXTVAL,
+        p_agenda_data
+    );
+
+END;
+
+-- Criando um programa que chama a procedure
+BEGIN
+    dbms_scheduler.create_program(program_name => 'HR.INSERIR_AGENDA_PRG', program_type => 'STORED_PROCEDURE', program_action => 'HR.INSERIR_AGENDA_PRC'
+    , number_of_arguments => 0, enabled => FALSE,
+                                 comments => 'Este e um programa que adiciona na tabela agenda');
+
+-- Habilitando o programa
+    dbms_scheduler.enable('HR.INSERIR_AGENDA_PRG');
+END;
+
+-- Dropando o programa
+BEGIN
+    dbms_scheduler.drop_program(program_name => 'HR.INSERIR_AGENDA_PRG', force => TRUE);
+END;
