1348a086c3fcde6c7492fed921e9bba6939ad9eb
Mauros Milach Junior <mauros99milach@gmail.com>
2024-08-18 19:19:30 -0300

    feat: Activity 9 - Creating a table with BFILE.sql

---
+++ Advanced - LOBs (Large Objects)/Activity 9 - Creating a table with BFILE.sql
@@ -0,0 +1,10 @@
+--@Author: MaurosMJ
+
+CREATE TABLE job_profiles (
+    profile_id      NUMBER,
+    first_name      VARCHAR2(200),
+    last_name       VARCHAR2(200),
+    profile_picture BFILE
+);
+--Criando uma sequence para job_profiles
+CREATE SEQUENCE job_profiles_seq START WITH 1 INCREMENT BY 1 NOCACHE NOMAXVALUE NOCYCLE;
